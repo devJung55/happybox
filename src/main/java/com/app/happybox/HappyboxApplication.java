b8842ef103806b2cd3175f7e2f403401c3fbdc96
dev-taeyang <oty7942@gmail.com>
2023-04-14 09:19:06 +0900

    initial settings

---
+++ src/main/java/com/app/happybox/HappyboxApplication.java
@@ -0,0 +1,13 @@
+package com.app.happybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/happybox/HappyboxApplicationTests.java
@@ -0,0 +1,12 @@
+package com.app.happybox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyboxApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
