8d8c9aa1359e1519436079e0818a40d8bdc668e7
devJung55 <devJung55@gmail.com>
2023-04-17 23:03:28 +0900

    정지영 - 마이페이지 퍼블리싱

---
+++ src/main/resources/static/js/mypage/review.js
@@ -0,0 +1,17 @@
+/* review.html */
+
+/* 좋아요 클릭 이벤트 */
+
+const $likeButtons = $(".like__button");
+
+$likeButtons.each((i, likeButton) => {
+    $(likeButton).on("click", function() {
+        if($(likeButton).attr("src") == "../../static/img/mypage/heart.png") {
+            $(likeButton).css("filter", "none");
+            $(likeButton).attr("src", "../../static/img/mypage/heart-pull.png");
+        } else {
+            $(likeButton).css("filter", "invert(100%) sepia(7%) saturate(722%) hue-rotate(198deg) brightness(118%)");
+            $(likeButton).attr("src", "../../static/img/mypage/heart.png");
+        }
+    });
+});
