4ed02ab2eea43631997bf0c9dc5f0bb1db4c17ec
devJung55 <devJung55@gmail.com>
2023-04-25 13:01:11 +0900

    정지영 - 마이페이지 퍼블리싱

--- src/main/resources/static/js/mypage/user/order-list.js
+++ src/main/resources/static/js/mypage/user/order-list.js
@@ -2,11 +2,14 @@
 
 /* 기간 클릭 이벤트 */
 const $radios = $(".custom-radio");
+let text = "";
 
 $radios.on("click", function() {
     $radios.each((i, radio) => {
         if($(radio).hasClass("default")) {
             $(radio).removeClass("default");
+            text = $($(this).children()[1]).text();
+            console.log(text);
         }
     });
     
