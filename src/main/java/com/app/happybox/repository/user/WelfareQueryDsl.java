44f0ee600347bdbd18d5781b29775b423691e806
dev-taeyang <oty7942@gmail.com>
2023-05-08 01:47:27 +0900

    오류 수정

--- src/main/java/com/app/happybox/repository/user/WelfareQueryDsl.java
+++ src/main/java/com/app/happybox/repository/user/WelfareQueryDsl.java
@@ -1,4 +1,25 @@
 package com.app.happybox.repository.user;
 
+import com.app.happybox.entity.user.Member;
+import com.app.happybox.entity.user.Welfare;
+import com.querydsl.core.Tuple;
+
+import java.util.Optional;
+
 public interface WelfareQueryDsl {
+
+    //    회원정보수정
+    public void setWelfareInfoById_QueryDSL(Welfare welfare);
+
+    //    회원탈퇴
+    public void setWelfareStatusById_QueryDSL(Welfare welfare);
+
+    //    로그인을 위해 ID로 Identification, Password 조회
+    public Tuple findWelfareInfoById(Long id);
+
+    //    가입여부 확인
+    public Optional<Member> findWelfareByWelfarePhone(String welfarePhone);
+
+    //    아이디 중복체크
+    public Boolean checkId(String welfareId);
 }
--- src/main/java/com/app/happybox/repository/user/WelfareQueryDslImpl.java
+++ src/main/java/com/app/happybox/repository/user/WelfareQueryDslImpl.java
@@ -1,9 +1,39 @@
 package com.app.happybox.repository.user;
 
+import com.app.happybox.entity.user.Member;
+import com.app.happybox.entity.user.Welfare;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class WelfareQueryDslImpl implements WelfareQueryDsl {
     private final JPAQueryFactory query;
+
+    @Override
+    public void setWelfareInfoById_QueryDSL(Welfare welfare) {
+
+    }
+
+    @Override
+    public void setWelfareStatusById_QueryDSL(Welfare welfare) {
+
+    }
+
+    @Override
+    public Optional<Member> findWelfareByWelfarePhone(String welfarePhone) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Tuple findWelfareInfoById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Boolean checkId(String welfareId) {
+        return null;
+    }
 }
