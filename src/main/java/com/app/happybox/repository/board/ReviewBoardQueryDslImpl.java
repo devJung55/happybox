f837d439457a743caef8d2f2e1411d887b713b2a
dennisjeong <jiwook6273@gmail.com>
2023-05-07 22:41:35 +0900

    리뷰 보드 레포지토리 작업 중

--- src/main/java/com/app/happybox/repository/board/ReviewBoardQueryDslImpl.java
+++ src/main/java/com/app/happybox/repository/board/ReviewBoardQueryDslImpl.java
@@ -1,26 +1,64 @@
 package com.app.happybox.repository.board;
 
 import com.app.happybox.entity.board.QReviewBoard;
-import com.app.happybox.entity.board.ReviewBoard;
-import com.app.happybox.entity.user.Member;
+import com.app.happybox.entity.board.QReviewBoardDTO;
+import com.app.happybox.entity.board.ReviewBoardDTO;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 
 import java.util.List;
 
-import static com.app.happybox.entity.board.QReviewBoard.reviewBoard;
+import static com.app.happybox.entity.board.QReviewBoard.*;
 
 @RequiredArgsConstructor
 public class ReviewBoardQueryDslImpl implements ReviewBoardQueryDsl {
     private final JPAQueryFactory query;
 
     @Override
-    public List<ReviewBoard> findReviewBoardListByMemberIdWithPaging_QueryDSL(Member member) {
-        List<ReviewBoard> reviewBoardList =  query.select(reviewBoard)
+    public Slice<ReviewBoardDTO> findAllByDateDescWithPaging_QueryDSL(Pageable pageable) {
+        List<ReviewBoardDTO> reviewBoardDTOS = query.select(new QReviewBoardDTO(
+                reviewBoard.id,
+                reviewBoard.member.memberName,
+                reviewBoard.subscription.welfare.welfareName,
+                reviewBoard.boardTitle,
+                reviewBoard.boardContent,
+                reviewBoard.updatedDate,
+                reviewBoard.reviewRating,
+                reviewBoard.reviewLikeCount.longValue(),
+                reviewBoard.reviewBoardReplies.size().longValue()
+        ))
                 .from(reviewBoard)
-                .where(reviewBoard.member.eq(member))
+                .orderBy(reviewBoard.updatedDate.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
                 .fetch();
 
-        return reviewBoardList;
+        return new SliceImpl<>(reviewBoardDTOS, pageable, true);
+    }
+
+    @Override
+    public Slice<ReviewBoardDTO> findAllByLikeCountDescWithPaging_QueryDSL(Pageable pageable) {
+        List<ReviewBoardDTO> reviewBoardDTOS = query.select(new QReviewBoardDTO(
+                reviewBoard.id,
+                reviewBoard.member.memberName,
+                reviewBoard.subscription.welfare.welfareName,
+                reviewBoard.boardTitle,
+                reviewBoard.boardContent,
+                reviewBoard.updatedDate,
+                reviewBoard.reviewRating,
+                reviewBoard.reviewLikeCount.longValue(),
+                reviewBoard.reviewBoardReplies.size().longValue()
+        ))
+                .from(reviewBoard)
+                .orderBy(reviewBoard.reviewLikeCount.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return new SliceImpl<>(reviewBoardDTOS, pageable, true);
     }
 }
