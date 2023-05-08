97b9a208b41113f2658d9945cbce2c9a4df834bf
devJung55 <devJung55@gmail.com>
2023-05-08 21:23:12 +0900

    정지영 - 마이페이지 주문내역, 취소내역 레포 작업 *^^*

--- src/main/java/com/app/happybox/repository/order/MemberOrderProductItemQueryDsl.java
+++ src/main/java/com/app/happybox/repository/order/MemberOrderProductItemQueryDsl.java
@@ -1,11 +1,13 @@
 package com.app.happybox.repository.order;
 
 import com.app.happybox.entity.order.MemberOrderProductItem;
-import com.app.happybox.entity.user.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface MemberOrderProductItemQueryDsl {
 //    마이페이지 주문내역
-    public Page<MemberOrderProductItem> findOrderListByMemberIdDescWithPaging_QueryDSL(Pageable pageable, Long id);
+    public Page<MemberOrderProductItem> findOrderListByMemberIdDescWithPaging_QueryDSL(Pageable pageable, Long memberId);
+
+//    마이페이지 취소내역
+    public Page<MemberOrderProductItem> findCancleListByMemberIdDescWithPaging_QueryDSL(Pageable pageable, Long memberId);
 }
--- src/main/java/com/app/happybox/repository/order/MemberOrderProductItemQueryDslImpl.java
+++ src/main/java/com/app/happybox/repository/order/MemberOrderProductItemQueryDslImpl.java
@@ -1,6 +1,8 @@
 package com.app.happybox.repository.order;
 
 import com.app.happybox.entity.order.MemberOrderProductItem;
+import com.app.happybox.entity.order.QMemberOrderProduct;
+import com.app.happybox.entity.type.PurchaseStatus;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -9,25 +11,42 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-import static com.app.happybox.entity.order.QMemberOrderProduct.memberOrderProduct;
 import static com.app.happybox.entity.order.QMemberOrderProductItem.memberOrderProductItem;
-import static com.app.happybox.entity.order.QProduct.product;
-import static com.app.happybox.entity.user.QMember.member;
 
 @RequiredArgsConstructor
 public class MemberOrderProductItemQueryDslImpl implements MemberOrderProductItemQueryDsl {
     private final JPAQueryFactory query;
 
     @Override
-    public Page<MemberOrderProductItem> findOrderListByMemberIdDescWithPaging_QueryDSL(Pageable pageable, Long id) {
+    public Page<MemberOrderProductItem> findOrderListByMemberIdDescWithPaging_QueryDSL(Pageable pageable, Long memberId) {
         List<MemberOrderProductItem> memberOrderProductItemList = query.select(memberOrderProductItem)
                 .from(memberOrderProductItem)
-                .join(memberOrderProduct).fetchJoin()
-                .join(product).fetchJoin()
-                .join(member).fetchJoin()
-                .where(memberOrderProductItem.memberOrderProduct.member.id.eq(id))
+                .join(memberOrderProductItem.memberOrderProduct).fetchJoin()
+                .join(memberOrderProductItem.product).fetchJoin()
+                .join(memberOrderProductItem.memberOrderProduct).fetchJoin()
+                .where(memberOrderProductItem.memberOrderProduct.member.id.eq(memberId))
+                .where(memberOrderProductItem.memberOrderProduct.purchaseStatus.eq(PurchaseStatus.CONFIRMED))
                 .orderBy(memberOrderProductItem.id.desc())
-                .offset(pageable.getOffset() - 1)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long count = query.select(memberOrderProductItem.id.count()).from(memberOrderProductItem).fetchOne();
+
+        return new PageImpl<>(memberOrderProductItemList, pageable, count);
+    }
+
+    @Override
+    public Page<MemberOrderProductItem> findCancleListByMemberIdDescWithPaging_QueryDSL(Pageable pageable, Long memberId) {
+        List<MemberOrderProductItem> memberOrderProductItemList = query.select(memberOrderProductItem)
+                .from(memberOrderProductItem)
+                .join(memberOrderProductItem.memberOrderProduct).fetchJoin()
+                .join(memberOrderProductItem.product).fetchJoin()
+                .join(memberOrderProductItem.memberOrderProduct).fetchJoin()
+                .where(memberOrderProductItem.memberOrderProduct.member.id.eq(memberId))
+                .where(memberOrderProductItem.memberOrderProduct.purchaseStatus.eq(PurchaseStatus.CANCELED))
+                .orderBy(memberOrderProductItem.id.desc())
+                .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
 
