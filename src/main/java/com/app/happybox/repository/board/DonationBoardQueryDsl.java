dbe9621591b00bada7861a9a99e90b31bc842b33
dennisjeong <jiwook6273@gmail.com>
2023-05-10 01:14:38 +0900

    레시피 댓글, 기부 게시판 레포지토리 작업

--- src/main/java/com/app/happybox/repository/board/DonationBoardQueryDsl.java
+++ src/main/java/com/app/happybox/repository/board/DonationBoardQueryDsl.java
@@ -1,13 +1,15 @@
 package com.app.happybox.repository.board;
 
 import com.app.happybox.entity.board.DonationBoard;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface DonationBoardQueryDsl {
-//    게시물 상세보기
-    public Optional<DonationBoard> findById_QueryDSL(Long id);
+//    기부 게시글 목록 페이징 처리
+    public Page<DonationBoard> findAllWithPaging(Pageable pageable);
 
 //    최신순 Top3 조회(메인)
     public List<DonationBoard> findTop3OrderByDate_QueryDSL();
