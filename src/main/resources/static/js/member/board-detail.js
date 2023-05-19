0291388bb998da9da45fac83699bec3f93ebf6b1
dennisjeong <jiwook6273@gmail.com>
2023-05-19 17:55:48 +0900

    레시피 작업 완료 직전

--- src/main/resources/static/js/member/board-detail.js
+++ src/main/resources/static/js/member/board-detail.js
@@ -143,7 +143,8 @@ function showDetail() {
                 <g id="Layer_1" />
               </svg>
             </button>
-            <button type="button" class="delete-btn">
+            <button type="button" class="delete-btn" onclick="deleteBoard()"
+            data-id="${review.id}">
               <svg viewBox="0 0 448 512" class="delete-button-icon" xmlns="http://www.w3.org/2000/svg">
                 <path
                 d="M432 80h-82.38l-34-56.75C306.1 8.827 291.4 0 274.6 0H173.4C156.6 0 141 8.827 132.4 23.25L98.38 80H16C7.125 80 0 87.13 0 96v16C0 120.9 7.125 128 16 128H32v320c0 35.35 28.65 64 64 64h256c35.35 0 64-28.65 64-64V128h16C440.9 128 448 120.9 448 112V96C448 87.13 440.9 80 432 80zM171.9 50.88C172.9 49.13 174.9 48 177 48h94c2.125 0 4.125 1.125 5.125 2.875L293.6 80H154.4L171.9 50.88zM352 464H96c-8.837 0-16-7.163-16-16V128h288v320C368 456.8 360.8 464 352 464zM224 416c8.844 0 16-7.156 16-16V192c0-8.844-7.156-16-16-16S208 183.2 208 192v208C208 408.8 215.2 416 224 416zM144 416C152.8 416 160 408.8 160 400V192c0-8.844-7.156-16-16-16S128 183.2 128 192v208C128 408.8 135.2 416 144 416zM304 416c8.844 0 16-7.156 16-16V192c0-8.844-7.156-16-16-16S288 183.2 288 192v208C288 408.8 295.2 416 304 416z"
@@ -184,6 +185,26 @@ function showDetail() {
 }
 
 showDetail();
+const goDelete = `/user-board/review-board-detail/delete/${review.id}`;
+/* 게시글 삭제 */
+function deleteBoard() {
+    $.ajax({
+        url: goDelete,
+        type: 'DELETE',
+        dataType: 'JSON',
+        success: function(result) {
+            console.log(result);
+            location.href = "/user-board/review-board-list";
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+}
+
+
+
+
 
 
 /* 댓글 관련 js */
@@ -326,6 +347,7 @@ $reviewOrder.on("click", function () {
 const $updateReviewBtn = $(".update_review");
 
 $updateReviewBtn.on("click", function () {
+    console.log("들어옴");
     /* 수정 중임을 의미하는 클래스 */
     const ON_UPDATE = "review-on-update";
 
@@ -339,7 +361,7 @@ $updateReviewBtn.on("click", function () {
             <textarea
                 class="write-textarea"
                 placeholder="댓글 남기기"
-            ></textarea
+            >${reply.content}</textarea
             ><button class="write-regist-btn" type="button">
                 <span class="regist">등록</span>
             </button>
