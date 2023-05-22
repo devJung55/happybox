c79ad9e48451f6cad78e183229300f7524e0b9fd
kmg2331 <kmg2331@gmail.com>
2023-05-23 01:10:43 +0900

    관리자 공지사항 수정, 삭제 완료

--- src/main/java/com/app/happybox/controller/mypage/MypageController.java
+++ src/main/java/com/app/happybox/controller/mypage/MypageController.java
@@ -7,6 +7,7 @@ import com.app.happybox.entity.board.RecipeBoardDTO;
 import com.app.happybox.entity.board.ReviewBoardDTO;
 import com.app.happybox.entity.file.UserFile;
 import com.app.happybox.entity.user.Member;
+import com.app.happybox.entity.user.User;
 import com.app.happybox.provider.UserDetail;
 import com.app.happybox.service.board.RecipeBoardLikeService;
 import com.app.happybox.service.board.RecipeBoardService;
@@ -47,31 +48,48 @@ public class MypageController {
     private final UserFileService userFileService;
     private final ReviewBoardService reviewBoardService;
 
-//    나의 구독 상세보기
+    //    userRole 에 따른 다른 경로로 redirect
+    @GetMapping("")
+    public RedirectView goMyPage(@AuthenticationPrincipal UserDetail userDetail) {
+        switch (userDetail.getUserRole()) {
+            case MEMBER:
+                return new RedirectView("/mypage/member/subscribe");
+            case WELFARE:
+                return new RedirectView("/mypage/welfare/rider/write");
+            case DISTRIBUTOR:
+                return new RedirectView("/mypage/distributor/product");
+            default:
+                return new RedirectView("/main/welfare");
+        }
+    }
+
+    //    나의 구독 상세보기
     @MypageHeaderValues
     @GetMapping("member/subscribe")
     public String getSubscribeDetail(@AuthenticationPrincipal UserDetail userDetail, Model model) {
         OrderSubscriptionDTO orderSubscriptionDTO = orderSubsciptionService.getSubscriptionDetailByMemberId(userDetail.getId());
         model.addAttribute("subscription", orderSubscriptionDTO);
-        if(orderSubscriptionDTO != null) {
+        if (orderSubscriptionDTO != null) {
             model.addAttribute("userProfile", userFileService.getDetail(orderSubscriptionDTO.getWelfareId()));
         }
         return "/mypage/member/subscribe";
     }
 
-//    구독 취소
+    //    구독 취소
     @ResponseBody
     @GetMapping("member/subscribe-cancel")
     public void cancelSubscribe(Long id) {
         orderSubsciptionService.cancelSubscribeById(id);
     }
 
-//    나의 게시물 목록(레시피)
+    //    나의 게시물 목록(레시피)
     @MypageHeaderValues
     @GetMapping("member/board")
-    public void getUserRecipeBoardList(@AuthenticationPrincipal UserDetail userDetail) {;}
+    public void getUserRecipeBoardList(@AuthenticationPrincipal UserDetail userDetail) {
+        ;
+    }
 
-//    나의 게시물 목록(레시피)
+    //    나의 게시물 목록(레시피)
     @ResponseBody
     @GetMapping("member/recipe-board")
     public Page<RecipeBoardDTO> getUserRecipeBoardList(@RequestParam(value = "page", defaultValue = "1", required = false) int page, @AuthenticationPrincipal UserDetail userDetail) {
@@ -79,12 +97,14 @@ public class MypageController {
         return recipeBoards;
     }
 
-//    나의 리뷰 목록
+    //    나의 리뷰 목록
     @MypageHeaderValues
     @GetMapping("member/review")
-    public void getReviewBoardList(@AuthenticationPrincipal UserDetail userDetail) {;}
+    public void getReviewBoardList(@AuthenticationPrincipal UserDetail userDetail) {
+        ;
+    }
 
-//    나의 리뷰 목록
+    //    나의 리뷰 목록
     @ResponseBody
     @GetMapping("member/review-board")
     public Page<ReviewBoardDTO> getReviewBoardList(@RequestParam(value = "page", defaultValue = "1", required = false) int page, @AuthenticationPrincipal UserDetail userDetail) {
@@ -92,12 +112,14 @@ public class MypageController {
         return reviewBoards;
     }
 
-//    나의 문의내역 목록
+    //    나의 문의내역 목록
     @MypageHeaderValues
     @GetMapping("member/inquiry")
-    public void getInquiryList(@AuthenticationPrincipal UserDetail userDetail) {;}
+    public void getInquiryList(@AuthenticationPrincipal UserDetail userDetail) {
+        ;
+    }
 
-//    나의 문의내역 목록
+    //    나의 문의내역 목록
     @ResponseBody
     @GetMapping("member/inquiry-list")
     public Page<InquiryDTO> getInquiryList(@RequestParam(value = "page", defaultValue = "1", required = false) int page, @AuthenticationPrincipal UserDetail userDetail) {
@@ -105,20 +127,20 @@ public class MypageController {
         return inquiries;
     }
 
-//    구매 내역 목록
+    //    구매 내역 목록
     @MypageHeaderValues
     @GetMapping("member/order")
-    public String getOrderList(@AuthenticationPrincipal UserDetail userDetail){
+    public String getOrderList(@AuthenticationPrincipal UserDetail userDetail) {
         return "/mypage/member/order-list";
     }
 
-//    구매 내역 목록
+    //    구매 내역 목록
     @ResponseBody
     @GetMapping("member/order-list")
     public Page<MemberOrderProductItemDTO> getOrderList(@RequestParam(value = "page", defaultValue = "1", required = false) int page, @AuthenticationPrincipal UserDetail userDetail, String year, String month, String day) {
         SearchDateDTO searchDateDTO = new SearchDateDTO();
 
-        if(year != null) {
+        if (year != null) {
             LocalDateTime setDate = LocalDateTime.of(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day), 0, 0);
             searchDateDTO.setSetDate(setDate);
         }
@@ -127,12 +149,14 @@ public class MypageController {
         return orderList;
     }
 
-//    레시피 찜 목록
+    //    레시피 찜 목록
     @MypageHeaderValues
     @GetMapping("member/recipe-bookmark")
-    public void getRecipeBookmarkList(@AuthenticationPrincipal UserDetail userDetail) {;}
+    public void getRecipeBookmarkList(@AuthenticationPrincipal UserDetail userDetail) {
+        ;
+    }
 
-//    레시피 찜 목록
+    //    레시피 찜 목록
     @ResponseBody
     @GetMapping("member/recipe-bookmark-list")
     public Page<RecipeBoardLikeDTO> getRecipeBookmarkList(@RequestParam(value = "page", defaultValue = "1", required = false) int page, @AuthenticationPrincipal UserDetail userDetail) {
@@ -140,21 +164,21 @@ public class MypageController {
         return bookmarkList;
     }
 
-//    레시피 찜 취소
+    //    레시피 찜 취소
     @ResponseBody
     @GetMapping("member/recipe-bookmark-cancel")
     public void calcelBookmarkRecipeBoard(Long id) {
         recipeBoardLikeService.cancelBookmarkRecipeById(id);
     }
 
-//    복지관 찜 목록
+    //    복지관 찜 목록
     @MypageHeaderValues
     @GetMapping("member/subscription-bookmark")
     public String getSubscriptionBookmarkList(@AuthenticationPrincipal UserDetail userDetail) {
         return "/mypage/member/welfare-bookmark";
     }
 
-//    복지관 찜 목록
+    //    복지관 찜 목록
     @ResponseBody
     @GetMapping("member/subscrition-bookmark-list")
     public Page<SubscriptionLikeDTO> getSubscriptionBookmarkList(@RequestParam(value = "page", defaultValue = "1", required = false) int page, @AuthenticationPrincipal UserDetail userDetail) {
@@ -162,7 +186,7 @@ public class MypageController {
 
         for (int i = 0; i < bookmarkList.getSize() - 1; i++) {
             for (int j = 0; j < userFileService.getList().size(); j++) {
-                if(userFileService.getList().get(j).getUser().getId() == bookmarkList.getContent().get(i).getWelfareId()) {
+                if (userFileService.getList().get(j).getUser().getId() == bookmarkList.getContent().get(i).getWelfareId()) {
                     bookmarkList.getContent().get(i).setUserFileDTO(userFileService.userFileToDTO(userFileService.getList().get(j)));
                 }
             }
@@ -170,14 +194,14 @@ public class MypageController {
         return bookmarkList;
     }
 
-//    복지관 찜 취소
+    //    복지관 찜 취소
     @ResponseBody
     @GetMapping("member/subscription-bookmark-cancel")
     public void calcelBookmarkSubscription(Long id) {
         subscriptionLikeService.cancelSubscriptionById(id);
     }
 
-//    회원 프로필 설정
+    //    회원 프로필 설정
     @ResponseBody
     @GetMapping("user/profile-update")
     public void updateProfile(@AuthenticationPrincipal UserDetail userDetail, String filePath, String fileUuid, String fileOrgName) {
@@ -185,7 +209,7 @@ public class MypageController {
         userFileService.registerProfile(userFile);
     }
 
-//    회원정보수정
+    //    회원정보수정
     @MypageHeaderValues
     @GetMapping("member/edit")
     public String updateMemberInfo(@AuthenticationPrincipal UserDetail userDetail, Model model) {
@@ -193,14 +217,14 @@ public class MypageController {
         return "/mypage/member/member-editor-form";
     }
 
-//    회원정보수정
+    //    회원정보수정
     @PostMapping("member/edit")
     public RedirectView updateMemberInfo(MemberDTO memberDTO) {
         memberService.updateMemberInfoById(memberDTO);
         return new RedirectView("/mypage/member/edit?update=ok");
     }
 
-//    배송지정보수정
+    //    배송지정보수정
     @MypageHeaderValues
     @GetMapping("member/address-editor")
     public String updateMemberDeliveryAddress(@AuthenticationPrincipal UserDetail userDetail, Model model) {
@@ -208,7 +232,7 @@ public class MypageController {
         return "/mypage/member/address-editor-form";
     }
 
-//    배송지정보수정
+    //    배송지정보수정
     @PostMapping("member/address-editor")
     public RedirectView updateMemberDeliveryAddress(MemberDTO memberDTO) {
         Member member = memberService.toMemberEntity(memberDTO);
@@ -216,28 +240,28 @@ public class MypageController {
         return new RedirectView("/mypage/member/address-editor?update=ok");
     }
 
-//    회원탈퇴
+    //    회원탈퇴
     @MypageHeaderValues
     @GetMapping("member/unregister")
     public String unregister(@AuthenticationPrincipal UserDetail userDetail) {
         return "/mypage/member/withdrawal";
     }
 
-//    회원탈퇴
+    //    회원탈퇴
     @PostMapping("member/unregister")
     public RedirectView unregisterPost(@AuthenticationPrincipal UserDetail userDetail) {
         userService.updateUserStatusByUserId(userDetail.getId());
         return new RedirectView("/login");
     }
 
-//    비밀번호 인증
+    //    비밀번호 인증
     @MypageHeaderValues
     @GetMapping("member/checkPassword")
     public String checkMemberPassword(@AuthenticationPrincipal UserDetail userDetail) {
         return "/mypage/member/member-editor";
     }
 
-//    비밀번호 인증
+    //    비밀번호 인증
     @PostMapping("member/checkPassword")
     public RedirectView checkMemberPassword(String password) {
         return new RedirectView("/mypage/member/member-editor-form");
