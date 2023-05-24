3d3ca0b6f860821f396b7d9c7617c90958c99929
zerojun <test.zerojune@gmail.com>
2023-05-24 13:01:21 +0900

    OAuth로 회원가입 및 OAuth 로그인 시 userDetail에 값 담아주기

--- src/main/java/com/app/happybox/provider/UserDetail.java
+++ src/main/java/com/app/happybox/provider/UserDetail.java
@@ -6,32 +6,46 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Map;
 
 @Component
 @Getter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class UserDetail implements UserDetails {
+public class UserDetail implements UserDetails, OAuth2User {
 
     private Long id;
     private String userId;
     private String userPassword;
     private Role userRole;
     private Collection<? extends GrantedAuthority> authorities;
+    /* OAuth용 */
+    private Map<String, Object> attributes;
 
     @Builder
-    public UserDetail(Long id, String userId, String userPassword, Role userRole, Collection<? extends GrantedAuthority> authorities) {
+    public UserDetail(Long id, String userId, String userPassword, Role userRole, Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes) {
         this.id = id;
         this.userId = userId;
         this.userPassword = userPassword;
         this.userRole = userRole;
         this.authorities = authorities;
+        this.attributes = attributes;
     }
 
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
